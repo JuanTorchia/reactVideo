48a58c2f66a4b2d77080439c7c44e2ac5a25bf43
Juan Torchia <j.s.torchia@gmail.com>
2021-01-09 15:54:16 -0300

    Validando LogIn y LogOut

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,34 +1,47 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import gravatar from '../utils/gravatar';
 import '../assets/styles/components/Header.scss';
 import logo from '../assets/static/logo-platzi-video-BW2.png';
 import userIcon from '../assets/static/user-icon.png';
 
-const Header = () => (
-  <header className='header'>
+const Header = (props) => {
+  const { user } = props;
+  const hasUser = Object.keys(user).length > 0;
+  return (
+    <header className='header'>
 
-    <Link to='/'>
-      <img className='header__img' src={logo} alt='Platzi Video' />
-    </Link>
-    <div className='header__menu'>
-      <div className='header__menu--profile'>
-        <img src={userIcon} alt='' />
-        <p>Perfil</p>
-      </div>
-      <ul>
-        <li>
-          <Link to='/login'>
+      <Link to='/'>
+        <img className='header__img' src={logo} alt='Platzi Video' />
+      </Link>
+      <div className='header__menu'>
+        <div className='header__menu--profile'>
+          {
+            hasUser ? <img src={gravatar(user.email)} alt={user.email} /> : <img src={userIcon} alt='' />
+          }
+          <p>Perfil</p>
+        </div>
+        <ul>
+          <li>
+            <Link to='/login'>
           Iniciar sesión
-          </Link>
-        </li>
-        <li>
-          <Link to='/Logout'>
+            </Link>
+          </li>
+          <li>
+            <Link to='/Logout'>
         Cerrar Sesión
-          </Link>
-        </li>
-      </ul>
-    </div>
-  </header>
-);
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </header>
+  );
+};
+const mapStateToProps = (state) => {
+  return {
+    user: state.user,
+  };
+};
 
-export default Header;
+export default connect(mapStateToProps, null)(Header);
