b75a54cdebcb2f532839eac27c7d3bd95a8644fa
Juan Torchia <j.s.torchia@gmail.com>
2021-01-09 16:12:43 -0300

    Se configuran las validaciones correspondiente para realizar logout, se configura propTypes para validar User y LogoutRequest

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import gravatar from '../utils/gravatar';
+import { logoutRequest } from '../actions';
 import '../assets/styles/components/Header.scss';
 import logo from '../assets/static/logo-platzi-video-BW2.png';
 import userIcon from '../assets/static/user-icon.png';
@@ -9,6 +11,10 @@ import userIcon from '../assets/static/user-icon.png';
 const Header = (props) => {
   const { user } = props;
   const hasUser = Object.keys(user).length > 0;
+
+  const handleLogout = () => {
+    props.logoutRequest({});
+  }
   return (
     <header className='header'>
 
@@ -23,16 +29,19 @@ const Header = (props) => {
           <p>Perfil</p>
         </div>
         <ul>
-          <li>
-            <Link to='/login'>
-          Iniciar sesión
-            </Link>
-          </li>
-          <li>
-            <Link to='/Logout'>
-        Cerrar Sesión
-            </Link>
-          </li>
+          {
+            hasUser ? <li><a href='/'>{user.name}</a></li> :
+              null
+          }
+          {
+            hasUser ?
+              <li><a href='#logout' onClick={handleLogout}>Cerrar Sesión</a></li> : (
+                <li>
+                  <Link to='/login'>
+                Iniciar sesión
+                  </Link>
+                </li>
+              )}
         </ul>
       </div>
     </header>
@@ -43,5 +52,13 @@ const mapStateToProps = (state) => {
     user: state.user,
   };
 };
+const mapDispatchToProps = {
+  logoutRequest,
+};
 
-export default connect(mapStateToProps, null)(Header);
+Header.propTypes = {
+  user: PropTypes.object,
+  logoutRequest: PropTypes.func.isRequired,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
